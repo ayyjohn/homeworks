63d3f35a7823ee85f1b22329fa2a73d2eada5c16
Alec Johnson <alecjohnson55@gmail.com>
2017-01-31 09:24:28 -0800

    Add playability to board

--- w2d2/lib/board.rb
+++ w2d2/lib/board.rb
@@ -1,16 +1,54 @@
+require_relative 'player'
+
 class Board
 
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @cups = Array.new(14) { [] }
+    setup_cups
+  end
+
+  def setup_cups
+    @cups.each_index do |index|
+      unless index == 6 || index == 13
+        @cups[index] = [:stone, :stone, :stone, :stone]
+      end
+    end
+  end
+
+  def valid_move?(start_pos)
+    raise "Invalid starting cup" unless start_pos.between?(1,13)
   end
 
   def place_stones
 
   end
 
-  def make_move(start_pos, current_player_name)
-
+  def make_move(start_pos, current_player)
+    i = start_pos + 1
+    until @cups[start_pos].empty?
+      i = 0 if i > 13
+      if current_player == "Erica"
+        @cups[i] << @cups[start_pos].pop unless i == 13
+        i += 1 unless @cups[start_pos].empty?
+      else
+        @cups[i] << @cups[start_pos].pop unless i == 6
+        i += 1 unless @cups[start_pos].empty?
+      end
+      i = 0 if i > 13
+    end
+    render
+    next_turn(i)
+    if cups[i].length == 1 && i != 13 && i != 6
+      :switch
+    elsif i == 6 && current_player == "Erica"
+      :prompt
+    elsif i == 13 && current_player == "James"
+      :prompt
+    else
+      i
+    end
   end
 
   def next_turn(ending_cup_idx)
@@ -27,11 +65,18 @@ class Board
   end
 
   def one_side_empty?
-
+    (0...6).all? { |idx| cups[idx].empty? } ||
+      (7...13).all? { |idx| cups[idx].empty? }
   end
 
   def winner
-
+    if cups[6].length > cups[13].length
+      "Erica"
+    elsif cups[6].length < cups[13].length
+      "James"
+    else
+      :draw
+    end
   end
 
 end
