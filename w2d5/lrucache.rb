0e5b2299003b97fba564d20e31c76707c076a4f5
Alec Johnson <alecjohnson55@gmail.com>
2017-02-02 23:16:31 -0800

    Impliment LRU cache class with array as the cache

--- w2d5/lrucache.rb
+++ w2d5/lrucache.rb
@@ -1,17 +1,26 @@
 class LRUCache
-    def initialize
+    def initialize(max_length)
+      @store = []
+      @max_length = max_length
     end
 
     def count
       # returns number of elements currently in cache
+      @store.count
     end
 
     def add(el)
       # adds element to cache according to LRU principle
+      @store.delete(el)
+      @store << el
+      if @store.length > @max_length
+        @store.shift
+      end
     end
 
     def show
       # shows the items in the cache, with the LRU item first
+      p @store
     end
 
     private
