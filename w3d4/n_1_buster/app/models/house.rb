71aef5939670f11e0fadd15b4dd8c1ab4e8fbd65
Alec Johnson <alecjohnson55@gmail.com>
2017-02-08 23:31:18 -0800

    Add better seeds

--- w3d4/n_1_buster/app/models/house.rb
+++ w3d4/n_1_buster/app/models/house.rb
@@ -24,5 +24,14 @@ class House < ActiveRecord::Base
 
   def better_seeds_query
     # TODO: your code here
+    plants = self.plants.includes(:seeds)
+
+    seeds = []
+
+    plants.each do |plant|
+      seeds << plant.seeds
+    end
+
+    seeds
   end
 end
