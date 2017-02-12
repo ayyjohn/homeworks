22ccb1fe4a8aaa92c0e479e8d90e991c24721a87
Alec Johnson <alecjohnson55@gmail.com>
2017-02-11 20:51:37 -0800

    Add functionality to add and delete books

--- w4d1/controllers/app/controllers/books_controller.rb
+++ w4d1/controllers/app/controllers/books_controller.rb
@@ -1,18 +1,25 @@
 class BooksController < ApplicationController
   def index
     # your code here
+    @books = Book.all
   end
 
   def new
-    # your code here
+    
   end
 
   def create
     # your code here
+    book = Book.new(book_params)
+    book.save
+    redirect_to :books
   end
 
   def destroy
     # your code here
+
+    Book.find(params[:id]).destroy
+    redirect_to :books
   end
 
   private
