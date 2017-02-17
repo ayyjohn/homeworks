8898b6407b4ea5e499cbbd220d7e367aa6b7114e
Alec Johnson <alecjohnson55@gmail.com>
2017-02-16 20:42:28 -0800

    Add toyable module and include it in corgi/cat

--- w4d5/ToyApp/app/models/corgi.rb
+++ w4d5/ToyApp/app/models/corgi.rb
@@ -10,5 +10,5 @@
 
 class Corgi < ActiveRecord::Base
 
-  has_many :toys, as: :toyable
+  include Toyable
 end
--- w4d5/ToyApp/app/models/toy.rb
+++ w4d5/ToyApp/app/models/toy.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: toys
+#
+#  id           :integer          not null, primary key
+#  name         :string           not null
+#  toyable_id   :integer
+#  toyable_type :string
+#
+
 class Toy < ActiveRecord::Base
 
   validates :name, uniqueness: { scope: [:toyable_id, :toyable_type] }
