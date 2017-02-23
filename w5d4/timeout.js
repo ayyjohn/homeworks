759711ed26759c564c5e3bde08cc61ba0505f6e4
Alec Johnson <alecjohnson55@gmail.com>
2017-02-22 22:28:22 -0800

    Finish javascript portion

---
+++ w5d4/timeout.js
@@ -0,0 +1,29 @@
+function hammerTime(time) {
+  setTimeout( () => console.log(`${time} is hammertime!`), 5000)
+}
+
+// hammerTime('6 pm');
+
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function teaAndBiscuits () {
+  reader.question('Would you like some tea?', function (res) {
+    console.log(`You replied ${res}.`);
+    reader.question('Would you like some biscuits?', function (res2) {
+      console.log(`You replied ${res2}.`);
+
+      const first = (res === 'yes') ? 'do' : 'don\'t';
+      const second = (res2 === 'yes') ? 'do' : 'don\'t';
+
+      console.log(`So you ${first} want tea and you ${second} want biscuits.`);
+      reader.close();
+    });
+  });
+}
+teaAndBiscuits();
